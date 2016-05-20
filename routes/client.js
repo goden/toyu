bb15eb614c08db645b3015b5bcf68d21432b65f6
Goden <goden.incredible@gmail.com>
2016-05-21 01:35:39 +0800

    完成客戶API的CRUD基本操作

--- routes/client.js
+++ routes/client.js
@@ -42,22 +42,104 @@ exports.read = function(req, res) {
 
 	model
 		.find(filter)
-		.populate('name')
+		// .populate('name')
 		// .populate('city', 'name')
-		// .populate('payments', 'comment')
+		// .populate('city', 'type')
+		.populate('payments', 'comment')
 		.exec(function(err, result) {
-			// res.send({
-			// 	result: result
-			// });
+			res.send({
+				result: result
+			});
 			console.log(result);
 			res.end();
 		});
 };
 
 exports.update = function(req, res) {
-	res.end();
+
+	// 客戶編號一旦建立，就不容再行更改
+	var model = req.app.db.model.Client;
+
+	var filter = {};
+	filter.cId = req.params.cId;
+	
+	var filter2Set = {};
+	filter2Set["$set"] = {};
+
+	if (typeof req.body.name !== "undefined") {
+		filter2Set["$set"].name=req.body.name;
+	}
+	if (typeof req.body.owner !== "undefined") {
+		filter2Set["$set"].owner=req.body.owner;
+	}
+	if (typeof req.body.ownerTitle !== "undefined") {
+		filter2Set["$set"].ownerTitle=req.body.ownerTitle;
+	}
+	if (typeof req.body.zip !== "undefined") {
+		filter2Set["$set"].zip=req.body.zip;
+	}
+	if (typeof req.body.city !== "undefined") {
+		filter2Set["$set"].city=req.body.city;
+	}
+	if (typeof req.body.address !== "undefined") {
+		filter2Set["$set"].address=req.body.address;
+	}
+	if (typeof req.body.tels !== "undefined") {
+		filter2Set["$set"].tels=req.body.tels;
+	}
+	if (typeof req.body.emails !== "undefined") {
+		filter2Set["$set"].emails=req.body.emails;
+	}
+	if (typeof req.body.faxes !== "undefined") {
+		filter2Set["$set"].faxes=req.body.faxes;
+	}
+	if (typeof req.body.website !== "undefined") {
+		filter2Set["$set"].website=req.body.website;
+	}
+	if (typeof req.body.taxNumber !== "undefined") {
+		filter2Set["$set"].taxNumber=req.body.taxNumber;
+	}
+	if (typeof req.body.comment !== "undefined") {
+		filter2Set["$set"].comment=req.body.comment;
+	}
+	if (typeof req.body.payments !== "undefined") {
+		filter2Set["$set"].payments=req.body.payments;
+	}
+	if (typeof req.body.accounts !== "undefined") {
+		filter2Set["$set"].accounts=req.body.accounts;
+	}
+
+	console.log(filter2Set);
+
+	model
+		.update(filter, filter2Set)
+		.exec(function(err, nModified) {
+			if (err) {
+				res.send({
+					result: {
+						successful: false
+					}
+				});
+			} else {
+				res.send({
+					successful: true,
+					nModified: nModified
+				});
+			}
+
+			res.end();
+		});
 };
 
 exports.delete = function(req, res) {
+
+	var model = req.app.db.model.Client;
+
+	model.remove({cId: req.params.cId});
+	res.send({
+		result: {
+			successful: true
+		}
+	});
 	res.end();
 };
